d82c8136e42107a888e64b6492010b9389f315ea
Wen Bin <wenbin_ng@yahoo.com.sg>
2026-04-14 22:43:25 +0800

    chore: setup data grid page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { App } from "./App";
+import { DataGrid } from "./pages/DataGrid";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/data-grid" element={<DataGrid />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>,
+);
---
+++ src/pages/DataGrid.tsx
@@ -0,0 +1,3 @@
+export const DataGrid = () => {
+    return <div>Data Grid</div>;
+};
